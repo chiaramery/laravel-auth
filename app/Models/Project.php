5c6e635ddc75bf1cc9b0ebe6e6807c49e6b83d9f
chiaramery <meringolachiara@gmail.com>
2023-01-13 12:41:14 +0100

    first commit

---
+++ app/Models/Project.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model {
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'slug'];
+
+    public static function generateSlug($title) {
+        return Str::slug($title, '-');
+    }
+}
---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <h2 class="text-center">Aggiungi un nuovo progetto</h2>
+        <div class="row justify-content-center">
+            <div class="col-8">
+                @include('partials.errors')
+                <form action="{{ route('admin.projects.store') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title">Titolo</label>
+                        <input type="text" id="title" name="title" class="form-control" value="{{ old('title') }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description">Descrizione</label>
+                        <textarea name="description" id="description" rows="10" class="form-control">{{ old('description') }}</textarea>
+                    </div>
+                    <button class="btn btn-success" type="submit">Salva</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
